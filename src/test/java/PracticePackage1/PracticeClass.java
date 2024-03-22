0e280def49cc63f0b9a7d178779812bfb833bfcd
ankur <ankurp1481995@gmail.com>
2024-03-22 22:52:01 +0530

    .

---
+++ src/test/java/PracticePackage1/PracticeClass.java
@@ -0,0 +1,13 @@
+package PracticePackage1;
+
+import org.testng.annotations.Test;
+
+public class PracticeClass {
+
+	@Test
+	
+	public void PracticeMethod()
+	{
+		System.out.println("dosa");
+	}
+}
